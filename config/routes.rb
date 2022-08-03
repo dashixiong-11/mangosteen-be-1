b7c855ad3063486b43a0360e6795997a80cd7376
dashixiong-11 <2788347834@qq.com>
2022-08-03 15:49:08 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show'
+
+  namespace :api do
+    namespace :vi do
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:create]
+      resources :items #exclude:[:create] 排除某个方法 
+      resources :tags
+    end
+  end
+
 end
+
